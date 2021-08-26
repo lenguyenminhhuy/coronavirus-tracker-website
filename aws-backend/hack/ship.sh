42d6da18ae20bc08f1a77f5755c5b2ee13798f62
CallMeDKay <s3812649@rmit.edu.vn>
2021-08-27 00:52:14 +0700

    :hammer: develop daily trigger sending email

---
+++ aws-backend/hack/ship.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#### Ship process ####
+ship_process() {
+  local mode="${1}"
+  if [ "${mode}" == "dev" ] 
+  then
+    echo "Start the DEV build process of .aws-sam folder..."
+    sam.cmd build -t template-dev.yaml
+    echo "=> Build DEV completed! ✅"
+    echo "Start the deployment process to AWS CloudFormation..."
+    sam deploy --config-file samconfig-dev.toml
+  else
+    echo "Start the PROD build process of .aws-sam folder..."
+    sam.cmd build
+    echo "=> Build PROD completed! ✅"
+    echo "Start the deployment process to AWS CloudFormation..."
+    sam.cmd deploy
+  fi
+}
+
+ship() {
+  local mode="${1}"
+  ship_process "${mode}"
+}
+
+
+err() {
+    echo "$*" >&2
+}
+
+usage() {
+    err "$(basename "$0"): [dev|prod]"
+}
+
+execute() {
+  local task=${1}
+  case "${task}" in
+    dev)
+      ship "dev"
+      ;;
+    prod)
+      ship "prod"
+      ;;
+    *)
+      err "invalid mode: ${task}"
+      usage
+      exit 1
+      ;;
+  esac
+}
+
+main() {
+  if [ $# -ne 1 ]
+  then
+    usage;
+    exit 1;
+  fi
+  local task=${1}
+  execute "${task}"
+}
+
+main "$@"
---
+++ aws-backend/src/handlers/send-daily-email.ts
@@ -0,0 +1,61 @@
+import "source-map-support";
+import aws from "aws-sdk";
+import { SendEmailRequest, SendEmailResponse } from "aws-sdk/clients/ses";
+
+// // Provide credentials
+// aws.config.loadFromPath("../../config.json");
+
+const ses = new aws.SES();
+exports.handler = async (): Promise<void> => {
+  // Specify e-mail agents
+  const sender = "CoronaNews <coronanews@minhthings.com>";
+  const recipient = "minhlucky2408@gmail.com"; // @test account
+
+  // Subject line of e-mail
+  const subject = "[Corona Headlines] HOT/BREAKING News Today!";
+
+  // Content of e-mail
+  const bodyHtml = `<html>
+  <head></head>
+  <body>
+    <h1>HOT/BREAKING News Today!</h1>
+    <p>
+      Enjoy the news!
+    </p>
+    <br/>
+    <b>Coronavirus-Tracker</b>
+  </body>
+  </html>`;
+
+  // charset encoding for e-mail
+  const charset = "UTF-8";
+
+  // Specify parameters to pass to the API
+  const mailParams: SendEmailRequest = {
+    Source: sender,
+    Destination: {
+      ToAddresses: [recipient],
+    },
+    Message: {
+      Subject: {
+        Data: subject,
+        Charset: charset,
+      },
+      Body: {
+        Html: {
+          Data: bodyHtml,
+          Charset: charset,
+        },
+      },
+    },
+  };
+
+  // Send Email operation
+  ses.sendEmail(mailParams, (err, data: SendEmailResponse) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("Email sent! Message ID: ", data.MessageId);
+    }
+  });
+};
