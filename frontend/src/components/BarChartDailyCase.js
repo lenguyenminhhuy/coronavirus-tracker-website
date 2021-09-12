a244492dedb8a3936b9f16b5972b9c55281ace04
hungnguyen <hungthezorba@gmail.com>
2021-09-12 20:43:38 +0700

    refactor: second daily chart

--- frontend/src/components/BarChartDailyCase.js
+++ frontend/src/components/BarChartDailyCase.js
@@ -12,13 +12,14 @@ import {
   Brush,
   Line
 } from "recharts";
-import { Box, Select, RadioGroup, Stack, Radio, Flex } from '@chakra-ui/react'
+import { Box, Select, RadioGroup, Stack, Radio, Flex, HStack, Checkbox } from '@chakra-ui/react'
 import CustomAxisX from "./shared/CustomAxisX";
 import CustomTooltip from "./shared/CustomTooltip";
 import normalizeCamelCase from "../utils/normalizeCamelCase";
 import axios from 'axios'
 import colors from '../constants/colors';
 import Loading from './Loading';
+import axiosCovid from "../config/axiosCovid";
 
 function sortByAlphabet(array, key) {
   return array.sort((a,b) => {
@@ -28,33 +29,47 @@ function sortByAlphabet(array, key) {
   })
 }
 
+function getMaxValue(array, type) {
+  return Math.max.apply(Math, array.map((o) => {
+    return o[type];
+  }))
+}
+
 function BarChartDailyCase({
-  data,
-  mode
+  mode,
+  currentLocationISO3
 }) {
 
-  const [historyData, setHistoryData] = useState([]);
+  const [historyData, setHistoryData] = useState(null);
   const [countryList, setCountryList] = useState("");
-  const [countryHistoryData, setCountryHistoryData] = useState("");
+  const [countryHistoryData, setCountryHistoryData] = useState(currentLocationISO3);
   const [radio, setRadio] = useState("totalCases");
   const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState({
+    totalTests: true,
+    totalVaccinations: true,
+    totalCases: true,
+    totalDeaths: false,
+  })
 
 
   useEffect(() => {
-    setLoading(true);
-    axios.get("https://79dvu6wjq3.execute-api.us-east-2.amazonaws.com/Prod/api/countries")
-    .then((res) => {
-        setCountryList(sortByAlphabet(res.data,'location'))
-        setCountryHistoryData(res.data[0].country)
-        setLoading(false);
-    })
+    console.log('dsss: ', currentLocationISO3);
+      setLoading(true);
+      axiosCovid.get("/api/countries")
+      .then((res) => {
+          setCountryList(sortByAlphabet(res.data,'location'))
+          setLoading(false);
+          if (currentLocationISO3 === null) {
+            setCountryHistoryData(res.data[0].country)
+          }
+      })
     },[])
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`https://79dvu6wjq3.execute-api.us-east-2.amazonaws.com/Prod/api/history?country=${countryHistoryData}`).then((res) => {
+    axiosCovid.get(`/api/history?country=${countryHistoryData}`).then((res) => {
       setHistoryData(res.data.data)
-      console.log(res.data);
       setLoading(false);
     });
   }, [countryHistoryData]);
@@ -77,7 +92,7 @@ function BarChartDailyCase({
           <Select borderTop="none" borderRight="none" borderLeft="none" w="40%" borderRadius={0} borderColor={colors.grayDefault} onChange={(value) => setCountryHistoryData(value.target.value)}>
               {countryList?
                   countryList.map(c => {
-                    if (c.location === countryHistoryData)
+                    if (c.country === countryHistoryData)
                       return <option selected key={c.country} value={c.country}>{c.location}</option>
                     else
                       return <option key={c.country} value={c.country}>{c.location}</option>
@@ -94,7 +109,7 @@ function BarChartDailyCase({
             </Stack>
           </RadioGroup>
         </Flex>
-<ResponsiveContainer width="100%" height="100%">
+        <ResponsiveContainer width="100%" height="100%" aspect={2}>
       <ComposedChart
         width="100%"
         height="100%"
@@ -110,12 +125,17 @@ function BarChartDailyCase({
           dataKey="date"
           tick={<CustomAxisX mode="date" />}
         />
-        <YAxis tickFormatter={(value) => value.toLocaleString()} />
+        {historyData?
+        <YAxis domain={[0, historyData[0][radio]? getMaxValue(historyData, radio) : 'dataMax']} allowDataOverflow={true} tickFormatter={(value) => value.toLocaleString()} />
+          :
+          null
+        }
         <Tooltip content={<CustomTooltip />} />
         <Legend />        
         <Bar type="monotone" dataKey={radio} name={normalizeCamelCase(radio)} fill="#82ca9d" />
       </ComposedChart>
     </ResponsiveContainer>
+
     </Box>
       
       </Box>
@@ -135,13 +155,50 @@ function BarChartDailyCase({
       <Box opacity={loading? 0.2 : 1} w="100%" h="100%">
   <Select borderTop="none" borderRight="none" borderLeft="none" w="40%" borderRadius={0} borderColor={colors.grayDefault} onChange={(value) => setCountryHistoryData(value.target.value)}>
                 {countryList?
-                    countryList.map(c => (
-                        <option key={c.country} value={c.country}>{c.location}</option>
-                    ))
+                      countryList.map(c => {
+                        if (c.country === countryHistoryData)
+                          return <option selected key={c.country} value={c.country}>{c.location}</option>
+                        else
+                          return <option key={c.country} value={c.country}>{c.location}</option>
+                  
+                      })
                 :
                 null
                 }
-            </Select>
+        </Select>
+        <HStack>
+          <Checkbox
+            borderColor={colors.grayLight} borderWidth="2px"
+            isChecked={status.totalVaccinations}
+            onChange={(e) => setStatus({...status,totalVaccinations: e.target.checked})}
+          >
+            Total Vaccinations
+          </Checkbox>
+          <Checkbox
+                      borderColor={colors.grayLight} borderWidth="2px"
+
+            isChecked={status.totalTests}
+            onChange={(e) => setStatus({...status,totalTests: e.target.checked})}
+          >
+            Total Test
+          </Checkbox>
+          <Checkbox
+                      borderColor={colors.grayLight} borderWidth="2px"
+
+            isChecked={status.totalCases}
+            onChange={(e) => setStatus({...status,totalCases: e.target.checked})}
+          >
+            Total Test
+          </Checkbox>
+          <Checkbox
+                      borderColor={colors.grayLight} borderWidth="2px"
+
+            isChecked={status.totalDeaths}
+            onChange={(e) => setStatus({...status,totalDeaths: e.target.checked})}
+          >
+            Total Test
+          </Checkbox>
+        </HStack>
             <ResponsiveContainer width="100%" height="90%">
         <ComposedChart
           width="100%"
@@ -157,11 +214,33 @@ function BarChartDailyCase({
             dataKey="date"
             tick={<CustomAxisX mode="date" />}
           />
-          <YAxis tickFormatter={(value) => value.toLocaleString()} />
-          <Tooltip content={<CustomTooltip />} />
-          <Legend verticalAlign="top" />        
-          <Bar type="monotone" dataKey="totalTests" name={normalizeCamelCase("totalTests")} fill="#82ca9d" />
-            <Line type="monotone" dataKey="totalVaccinations" name={normalizeCamelCase("totalVaccinations")} color="#a2ca9d" />
+        {historyData?
+        <YAxis domain={[0, historyData[0]['totalVaccinations']? getMaxValue(historyData, 'totalVaccinations') : 'dataMax']} allowDataOverflow={true} tickFormatter={(value) => value.toLocaleString()} />
+          :
+          null
+        }
+          <Tooltip content={<CustomTooltip mode="date" />} />
+          <Legend verticalAlign="top" /> 
+          {status.totalVaccinations?
+            <Line type="monotone" dataKey="totalVaccinations" name={normalizeCamelCase("totalVaccinations")} stroke={colors.oceanBlueDefault} />
+          :
+            null
+          }       
+          {status.totalTests?
+              <Line type="monotone" dataKey="totalTests" name={normalizeCamelCase("totalTests")} stroke={colors.grayDarkest} />
+            :
+            null
+          }
+          {status.totalCases?
+              <Line type="monotone" dataKey="totalCases" name={normalizeCamelCase("totalCases")} stroke={colors.yellowDefault} />
+            :
+            null
+          }     
+          {status.totalDeaths?
+              <Line type="monotone" dataKey="totalDeaths" name={normalizeCamelCase("totalDeaths")} stroke={colors.redDefault} />
+            :
+            null
+          }          
           <Brush y={310} dataKey="date" height={20} stroke={colors.redLight}/>
           </ComposedChart>
       </ResponsiveContainer>
